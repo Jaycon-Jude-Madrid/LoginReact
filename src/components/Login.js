62f18d0e94bf0e9ad6e59e280998c7b42f2fb145
Jaycon-Jude-Madrid <jaycon46madrid@gmail.com>
2022-02-07 20:40:59 +0800

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import './Login.css'
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import GoogleButton from "react-google-button";
+//import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext";
 
 const Login = () => {
@@ -23,7 +24,7 @@ const Login = () => {
     }
   };
 
-  const handleGoogleSignIn = async (e) => {
+  /*const handleGoogleSignIn = async (e) => {
     e.preventDefault();
     try {
       await googleSignIn();
@@ -31,48 +32,48 @@ const Login = () => {
     } catch (error) {
       console.log(error.message);
     }
-  };
+  };*/
 
   return (
     <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Firebase Auth Login</h2>
+      <div className="container">
+        <div className="parent-container">
+        <h2 className="logInText">Login </h2>
+        <h4 className="dashBoardText">Proceed to dashboard</h4>
+        <hr />
         {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className=" mb-3" controlId="formBasicEmail">
             <Form.Control
+      className="input-one"
               type="email"
-              placeholder="Email address"
+              placeholder="Email address . . ."
               onChange={(e) => setEmail(e.target.value)}
             />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="password mb-3" controlId="formBasicPassword">
             <Form.Control
+            className="input-two"
               type="password"
-              placeholder="Password"
+              placeholder="Password . . ."
               onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
 
           <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Log In
+            <Button className="Button" variant="primary" type="Submit">
+              Log in Admin
             </Button>
           </div>
         </Form>
-        <hr />
-        <div>
-          <GoogleButton
-            className="g-btn"
-            type="dark"
-            onClick={handleGoogleSignIn}
-          />
-        </div>
+  
+     
       </div>
-      <div className="p-4 box mt-3 text-center">
-        Don't have an account? <Link to="/signup">Sign up</Link>
-      </div>
+    {/* <div className="p-4 box mt-3 text-center">
+    Don't have an account? <Link to="/signup">Sign up</Link>
+      </div> */}
+           </div>
     </>
   );
 };
